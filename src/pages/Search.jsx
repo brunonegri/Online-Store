2aa2303a671c8ffe177f13f0953a41f906c8cc64
Ana Carolina Magalhaes <carol2015bortolini@gmail.com>
2022-05-24 18:00:26 -0300

    Correcao search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -13,7 +13,10 @@ class Search extends React.Component {
           <label htmlFor="search">
             <input type="text" id="search" value={ valueInput } />
           </label>
-          { verificaInput ? <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
+          { verificaInput ? 
+            <p data-testid="home-initial-message">
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </p>
             : ''}
         </div>
       );
