852136eee59e5fc22a1462e51311549737188231
Mila Souza <milinhaps27@gmail.com>
2022-05-24 23:12:59 -0300

    Requisito 6 - feito

--- src/pages/ListCategories.jsx
+++ src/pages/ListCategories.jsx
@@ -1,29 +1,57 @@
 import React from 'react';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
+import Products from '../components/Products';
 
 class ListCategories extends React.Component {
     state = {
       category: [],
+      prodList: [],
     }
 
     async componentDidMount() {
       const showCategories = await getCategories();
-      console.log(showCategories);
       this.setState({ category: showCategories });
     }
 
+    handleChange = async ({ target }) => {
+      const { value } = target;
+      const itensPorCategoria = await getProductsFromCategoryAndQuery(value);
+      this.setState({ prodList: itensPorCategoria.results });
+    }
+
     render() {
-      const { category } = this.state;
+      const { category, prodList } = this.state;
       return (
-        <fieldset>
-          <h1>Categoria:</h1>
-          {category.map(({ name }) => (
-            <label key={ name } htmlFor="category">
-              <input type="radio" name="category" data-testid="category" />
-              { name }
-            </label>
-          ))}
-        </fieldset>
+        <div>
+          <fieldset>
+            <h1>Categoria:</h1>
+            {category.map((item) => (
+              <label key={ item.name } htmlFor="category">
+                <input
+                  id="category"
+                  type="radio"
+                  name="category"
+                  data-testid="category"
+                  onChange={ this.handleChange }
+                  value={ item.id }
+                />
+                { item.name }
+              </label>
+            ))}
+          </fieldset>
+          <div>
+            {
+              prodList.map((item) => (
+                <Products
+                  key={ item.id }
+                  name={ item.title }
+                  imagem={ item.thumbnail }
+                  price={ item.price }
+                />
+              ))
+            }
+          </div>
+        </div>
       );
     }
 }
